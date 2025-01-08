6220b6aae4e62a9dbac7925a0dad0e44847648d7
Mo-Deck <mohamedabow6774@gmail.com>
2025-01-08 09:56:36 +0300

    Building the timer Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import Header from "./components/header/Header"
+import  Timer  from "./components/timer/Timer"
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Timer />
     </div>
   )
   
