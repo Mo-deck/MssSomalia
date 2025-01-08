3cc61bd2070946e0d789df329d3bc047122e9f4f
Mo-Deck <mohamedabow6774@gmail.com>
2025-01-08 10:43:53 +0300

    styling Timer components

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -6,19 +6,19 @@ import styles from "./timer.module.scss";
         <h1>VOTING ENDS IN</h1>
         <div className={styles.time}>
             <div className={styles.digit_text}>
-                <span className={styles.digit}>06</span>
+                <span className={styles.digit}>04:</span>
                 <span className={styles.text}>Days</span>
             </div>
             <div className={styles.digit_text}>
-                <span className={styles.digit}>06</span>
-                <span className={styles.text}>Hour</span>
+                <span className={styles.digit}>09:</span>
+                <span className={styles.text}>Hours</span>
             </div>
             <div className={styles.digit_text}>
-                <span className={styles.digit}>06</span>
-                <span className={styles.text}>Miuntes</span>
+                <span className={styles.digit}>03:</span>
+                <span className={styles.text}>Minutes</span>
             </div>
             <div className={styles.digit_text}>
-                <span className={styles.digit}>06</span>
+                <span className={styles.digit}>05:</span>
                 <span className={styles.text}>Seconds</span>
             </div>
         </div>
