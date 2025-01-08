8d0530a212bb6150546fcc2b5d0db17b994c6cf1
Mo-Deck <mohamedabow6774@gmail.com>
2025-01-08 13:43:25 +0300

    building competitors components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react"
 import Header from "./components/header/Header"
 import  Timer  from "./components/timer/Timer"
+import Competitors from "./components/competitors/Competitors"
 function App() {
 
   return (
     <div className="App">
       <Header />
       <Timer />
+      <Competitors />
     </div>
   )
   
---
+++ src/components/competitors/Competitors.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Competitors() {
+  return 
+    <div className={styles.competitors_container}>
+        <div className={styles.competitors_header}>
+            <span>Best Comedians</span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet iste saepe, placeat nulla, sed corrupti expedita officia vitae cumque maxime, fugiat fugit tempora. At omnis aut cumque quibusdam optio illum?</p>
+        </div>
+    </div>
+}
+
+export default Competitors
