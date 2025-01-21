ea7de29874a64012219b43c2e31e0ed96f6d694d
Mo-Deck <mohamedabow6774@gmail.com>
2025-01-21 10:40:22 +0300

    Errorki wan xaliyey

--- src/components/competitor/Competitor.jsx
+++ src/components/competitor/Competitor.jsx
@@ -1,14 +1,19 @@
 import styles from "./Competitor.module.scss"
 
-function Competitor(competitor) {
+function Competitor({competitor}) {
   return (
-    <div className={styles.Competitor}>
-        <div className={styles.info}>
-            <span className={styles.name}>{competitor.FirstName}</span>
-            <span className={styles.state}>{competitor.RepresentingState}</span>
-            <span className={styles.vote_count}>
-                    Total Votes : {competitor.NumberofVotes}</span>
-        </div>
+    <div className={styles.competitor}>
+       <div className={styles.info}>
+        <span className={styles.name}>
+          {competitor.FirstName}
+        </span>
+        <span className={styles.state}>
+          {competitor.RepresentingState}
+        </span>
+        <span className={styles.vote_count}>
+          {competitor.NumberofVotes}
+        </span>
+       </div>
     </div>
   )
 }
