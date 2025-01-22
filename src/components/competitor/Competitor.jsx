f06ce6803f6065d224497c1f1aa1f2e90e4840b8
Mo-Deck <mohamedabow6774@gmail.com>
2025-01-22 21:45:39 +0300

    styling the competitor

--- src/components/competitor/Competitor.jsx
+++ src/components/competitor/Competitor.jsx
@@ -1,18 +1,28 @@
 import styles from "./Competitor.module.scss"
+import { MdHowToVote } from "react-icons/md";
 
 function Competitor({competitor}) {
+  const backgroundStyle ={
+    width: "100%",
+    background: `linear-gradient(0deg, #128b4871, rgba(0,0,0,0)60%,rgba(0,0,0,0)),url(${competitor.Photo})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat"
+  }
   return (
-    <div className={styles.competitor}>
+    <div className={styles.competitor}  style={backgroundStyle}>
        <div className={styles.info}>
         <span className={styles.name}>
-          {competitor.FirstName}
+          {competitor.FirstName + " " + competitor.LastName}
         </span>
         <span className={styles.state}>
           {competitor.RepresentingState}
         </span>
         <span className={styles.vote_count}>
-          {competitor.NumberofVotes}
+          Total Votes :{competitor.NumberofVotes}
         </span>
+       </div>
+       <div className={styles.vote}>
+        <MdHowToVote className={styles.vote_icon} />
        </div>
     </div>
   )
