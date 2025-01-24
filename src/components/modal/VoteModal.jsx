cbdf137f6bd619a692c4c7fe21b9d8176a54fdb0
Mo-Deck <mohamedabow6774@gmail.com>
2025-01-24 08:14:49 +0300

    VoteModal

--- src/components/modal/VoteModal.jsx
+++ src/components/modal/VoteModal.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
 import styles from './vote.module.scss'
+import { FaPlus, FaMinus } from "react-icons/ai";
 
 
 const customStyles = {
@@ -14,16 +15,6 @@ const customStyles = {
   },
 };
 
-const backgroundStyle ={
-  content: {
-    top:"50%",
-    left: "50%",
-    right: auto,
-    bottom: auto,
-    marginRight:"-50%",
-    transform: "translate(-50%, -50%)"
-  }
-}
 Modal.setAppElement('#root');
 
 export const VoteModal = () => {
@@ -44,6 +35,15 @@ export const VoteModal = () => {
       setIsOpen(false);
     }  
 
+    const backgroundStyle ={
+      width: "100%",
+      height: "500px",
+      background: `linear-gradient(0deg, #29ae56b2, rgba(0,0,0,0)60%, rgba(0,0,0,0)), url('https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D')`,
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      borderButtomRightRadius: "10px"
+     };
+
   return(
 
     <div>
@@ -60,7 +60,26 @@ export const VoteModal = () => {
 
             <div className={styles.competitor_info}>
               <div style={backgroundStyle}></div>
+              <div className={styles.bio}>
+                <div className={styles.divider}>
+                  <label htmlFor="">Name</label>
+                  <span>Farhio Cabdi</span>
+                </div>
+                <div className={styles.divider}>
+                  <label htmlFor="">State</label>
+                  <span>Jubbaland</span>
+                </div>
+                <div className={styles.divider}>
+                  <label htmlFor="">Background Study</label>
+                  <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident voluptas repudiandae quas perferendis similique maxime ipsam error quo nihil, quasi alias. Amet, illo. Alias doloremque voluptatem aliquam quo ex impedit?</span>
+                </div>
+                <div className={styles.divider}>
+                  <label htmlFor="">Employement</label>
+                  <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident voluptas repudiandae quas perferendis similique maxime ipsam error quo nihil, quasi alias. Amet, illo. Alias doloremque voluptatem aliquam quo ex impedit?</span>
+                </div>
+              </div>
             </div>
+
 
           </div>
       </Modal>
